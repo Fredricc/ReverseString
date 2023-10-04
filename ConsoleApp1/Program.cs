324a5002db0767c4edf8734f4cd71b8ce4b55856
USER <USER@FREDDIE>
2023-10-04 03:59:17 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string name = Console.ReadLine();
+
+        string reverseString = StringToReverse(name);
+        Console.WriteLine(reverseString);
+    }
+
+    private static string StringToReverse(string? name)
+    {
+        string r = "";
+
+        for (int i = name.Length - 1; i >= 0 ; i--)
+        {
+            r += name[i];
+        }
+        return r;
+    }
+}

721969b80d3bbae86480ca7f0669a1e1de528ab2
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-10-13 21:11:52 +0300

    strings

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Enter your name that will be printed");
+        Console.WriteLine("Enter your name  that will be printed");
         string name = Console.ReadLine();
 
         string reverseString = StringToReverse(name);
